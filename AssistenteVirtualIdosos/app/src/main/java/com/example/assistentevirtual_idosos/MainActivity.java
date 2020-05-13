5c8ac069cc50058b6919dbe6180c2371834e1ef4
victorcitro <victorcitrovelozo@hotmail.com>
2020-05-12 21:32:22 -0300

    metodo oncreat/botao microfone

--- AssistenteVirtualIdosos/app/src/main/java/com/example/assistentevirtual_idosos/MainActivity.java
+++ AssistenteVirtualIdosos/app/src/main/java/com/example/assistentevirtual_idosos/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.assistentevirtual_idosos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView (R.layout.activity_main);
+        findViewById(R.id.microphone).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
     }
 }
