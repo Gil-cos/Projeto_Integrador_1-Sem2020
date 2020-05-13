a4c92c8ac5b4f874fe838a07f0efed9bd99b830d
Gabriel Soares Gonçalves <gabrielsoaresg20@gmail.com>
2020-05-12 21:38:11 -0300

    Implementação do Método CatchSpeech

--- AssistenteVirtualIdosos/app/src/main/java/com/example/assistentevirtual_idosos/MainActivity.java
+++ AssistenteVirtualIdosos/app/src/main/java/com/example/assistentevirtual_idosos/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.assistentevirtual_idosos;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,8 +20,22 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.microphone).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                catchSpeech();
             }
         });
 
+    }
+    private void catchSpeech() {
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.US);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Fale naturalmente...");
+
+        try{
+            startActivityForResult(intent, 10);
+        }catch (ActivityNotFoundException a){
+            Toast.makeText(this,"Reconhecimento de voz não suportado",Toast.LENGTH_SHORT).show();
+        }
     }
 }
