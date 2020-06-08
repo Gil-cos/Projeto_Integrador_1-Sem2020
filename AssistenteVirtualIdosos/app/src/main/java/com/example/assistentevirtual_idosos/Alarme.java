d9512be9fbda016afce1707f22859ffedbb911a4
victorcitro <victorcitrovelozo@hotmail.com>
2020-06-08 09:23:23 -0300

    Adicionando recurso de voz Activity

--- AssistenteVirtualIdosos/app/src/main/java/com/example/assistentevirtual_idosos/Alarme.java
+++ AssistenteVirtualIdosos/app/src/main/java/com/example/assistentevirtual_idosos/Alarme.java
@@ -1,5 +1,8 @@
 package com.example.assistentevirtual_idosos;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +11,8 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 public class Alarme extends AppCompatActivity {
 
@@ -19,6 +24,13 @@ public class Alarme extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarme);
 
+        findViewById(R.id.microphonee).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                catchSpeech();
+            }
+        });
+
         segunda = findViewById(R.id.segunda);
         terça = findViewById(R.id.terça);
         quarta = findViewById(R.id.quarta);
@@ -26,13 +38,23 @@ public class Alarme extends AppCompatActivity {
         sexta = findViewById(R.id.sexta);
         sabado = findViewById(R.id.sabado);
         domingo = findViewById(R.id.domingo);
-
-
     }
+
     public void checkbox () {
         boolean teste = segunda.isChecked();
-
-
     }
 
+    private void catchSpeech() {
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.US);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Fale naturalmente...");
+
+        try {
+            startActivityForResult(intent, 10);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(this, "Reconhecimento de voz não suportado", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
