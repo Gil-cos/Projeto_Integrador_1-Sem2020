2cf09c6e0fefe1647aec64855813184d43a99527
Gabriel Ferraz <bi.henriquesf@hotmail.com>
2020-06-18 19:33:44 -0300

    layout da funcionalidade

---
+++ AssistenteVirtualIdosos/app/src/main/java/com/example/assistentevirtual_idosos/activityEvent.java
@@ -0,0 +1,13 @@
+package com.example.assistentevirtual_idosos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activityEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+    }
+}
